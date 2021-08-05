07986458767c9fdc895b4ccf262cabee2f8fd60e
Anh Tu Le <anh.le@vertics.cogit config --global user.email anh.le@vertics.co>
2021-08-05 21:04:31 +0300

    feat: update transform for corelayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import CoreLayout from './shared/layout/CoreLayout';
 export default function App() {
-  return (
-    <div className="app">
-      <CoreLayout />
-    </div>
-  );
+  return <CoreLayout />;
 }
