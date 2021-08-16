fe0faf82ff7e84000d99d40d138670ad699c765c
qp1993 <52257087+qp1993@users.noreply.github.com>
2021-08-16 15:40:41 -0700

    feat: designed & restructured history page

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Timeline from "@material-ui/lab/Timeline";
+
 import Event from "../shared/components/Event";
 import data from "../shared/components/HistoryData";
 
@@ -7,50 +9,59 @@ export default function History() {
   return (
     <div className="history">
       <div className="history-education">
-        <Event
-          name={data[0].name}
-          position={data[0].position}
-          timeframe={data[0].timeframe}
-          paragraph={data[0].paragraph}
-        />
-        <Event
-          name={data[1].name}
-          position={data[1].position}
-          timeframe={data[1].timeframe}
-          paragraph={data[1].paragraph}
-        />
-        <Event
-          name={data[2].name}
-          position={data[2].position}
-          timeframe={data[2].timeframe}
-          paragraph={data[2].paragraph}
-        />
+        <h3 className="title">Education</h3>
+        <Timeline align="left">
+          <Event
+            name={data[0].name}
+            position={data[0].position}
+            timeframe={data[0].timeframe}
+            paragraph={data[0].paragraph}
+            isLeft={true}
+          />
+          <Event
+            name={data[1].name}
+            position={data[1].position}
+            timeframe={data[1].timeframe}
+            paragraph={data[1].paragraph}
+            isLeft={true}
+          />
+          <Event
+            name={data[2].name}
+            position={data[2].position}
+            timeframe={data[2].timeframe}
+            paragraph={data[2].paragraph}
+            isLeft={true}
+          />
+        </Timeline>
       </div>
       <div className="history-work">
-        <Event
-          name={data[3].name}
-          position={data[3].position}
-          timeframe={data[3].timeframe}
-          paragraph={data[3].paragraph}
-        />
-        <Event
-          name={data[4].name}
-          position={data[4].position}
-          timeframe={data[4].timeframe}
-          paragraph={data[4].paragraph}
-        />
-        <Event
-          name={data[5].name}
-          position={data[5].position}
-          timeframe={data[5].timeframe}
-          paragraph={data[5].paragraph}
-        />
-        <Event
-          name={data[6].name}
-          position={data[6].position}
-          timeframe={data[6].timeframe}
-          paragraph={data[6].paragraph}
-        />
+        <h3 className="title">Work History</h3>
+        <Timeline align="right">
+          <Event
+            name={data[3].name}
+            position={data[3].position}
+            timeframe={data[3].timeframe}
+            paragraph={data[3].paragraph}
+          />
+          <Event
+            name={data[4].name}
+            position={data[4].position}
+            timeframe={data[4].timeframe}
+            paragraph={data[4].paragraph}
+          />
+          <Event
+            name={data[5].name}
+            position={data[5].position}
+            timeframe={data[5].timeframe}
+            paragraph={data[5].paragraph}
+          />
+          <Event
+            name={data[6].name}
+            position={data[6].position}
+            timeframe={data[6].timeframe}
+            paragraph={data[6].paragraph}
+          />
+        </Timeline>
       </div>
     </div>
   );
--- src/shared/components/Event.jsx
+++ src/shared/components/Event.jsx
@@ -1,14 +1,30 @@
 import React from "react";
-import { Fragment } from "react";
 
-const Event = ({ name, position, timeframe, paragraph }) => {
+import TimelineItem from "@material-ui/lab/TimelineItem";
+import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
+import TimelineConnector from "@material-ui/lab/TimelineConnector";
+import TimelineContent from "@material-ui/lab/TimelineContent";
+import TimelineDot from "@material-ui/lab/TimelineDot";
+
+const Event = ({ name, position, timeframe, paragraph, isLeft }) => {
   return (
-    <Fragment className="event">
-      <h3 className="event-name">{name}</h3>
-      <h4 className="event-position">{position}</h4>
-      <div className="event-timeframe">{timeframe}</div>
-      <p className="event-paragraph">{paragraph}</p>
-    </Fragment>
+    <TimelineItem>
+      <TimelineSeparator>
+        <TimelineDot />
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>
+        <div className="event">
+          <h3 className="event-name">{name}</h3>
+          <div className="event-header">
+            <h4 className="position">{position}</h4>
+            <div className="timeframe">{timeframe}</div>
+          </div>
+          <p className="event-paragraph">{paragraph}</p>
+          <div className={`event-arrow ${isLeft ? "left" : "right"}`}></div>
+        </div>
+      </TimelineContent>
+    </TimelineItem>
   );
 };
 
